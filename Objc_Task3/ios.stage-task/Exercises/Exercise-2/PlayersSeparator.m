4b18d970683e4ba20cbe9c928319f5640056519a
Alexandr Karensky <skittles8bit@gmail.com>
2021-05-22 13:59:15 +0300

    feat: implementation divide players into teams function

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,20 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int teams = 0;
+    
+    for (int i = 0; i < ratingArray.count; i++) {
+        for (int j = i + 1; j < ratingArray.count; j++) {
+            for (int k = j + 1; k < ratingArray.count; k++) {
+                if (((([ratingArray[i] intValue] < [ratingArray[j] intValue]) && ([ratingArray[j] intValue]) < [ratingArray[k] intValue])) ||
+                ((([ratingArray[i] intValue] > [ratingArray[j] intValue]) && ([ratingArray[j] intValue] > [ratingArray[k] intValue])))){
+                    teams++;
+                }
+            }
+        }
+    }
+    
+    return teams;
 }
 
 @end
