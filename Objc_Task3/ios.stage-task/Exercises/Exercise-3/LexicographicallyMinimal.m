6ddbdacd69737001b79dae2ca5b7b0c6208264d8
Alexandr Karensky <skittles8bit@gmail.com>
2021-05-24 10:38:45 +0300

    feat: implementation find lexic min for string fucntion

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,30 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *result = [NSMutableString new];
+    NSString *lineFirst = [NSString new];
+    NSString *lineSecond = [NSString new];
+    int stringFirstCount = 0;
+    int stringSecondCount = 0;
+    
+    while ((stringFirstCount < string1.length) && (stringSecondCount < string2.length)) {
+        lineFirst = [string1 substringWithRange:NSMakeRange(stringFirstCount, 1)];
+        lineSecond = [string2 substringWithRange:NSMakeRange(stringSecondCount, 1)];
+        
+        if ([lineFirst UTF8String][0] <= [lineSecond UTF8String][0]) {
+            [result appendString:lineFirst];
+            stringFirstCount++;
+        } else {
+            [result appendString:lineSecond];
+            stringSecondCount++;
+        }
+    }
+    
+    stringFirstCount == string1.length
+        ? [result appendString:[string2 substringFromIndex:stringSecondCount]]
+        : [result appendString:[string1 substringFromIndex:stringFirstCount]];
+    
+    return result;
 }
 
 @end
